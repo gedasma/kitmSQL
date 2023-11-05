ca88a90426f37a1a7363c16c54b05e630a96ce81
gedasma <gedasma@gmail.com>
2023-11-05 18:15:05 +0200

    filling files that i forgot to save

---
+++ KasVykstaManoMieste/FillTable/FillTable_Event.sql
@@ -0,0 +1,12 @@
+INSERT INTO Event (eventId, eventName, eventCategoryId, eventTime, eventLocation, eventPicture, eventCreatorUserId)
+VALUES
+    (0, 'Science Symposium', 1, '2023-11-05 10:00:00', 'Science Center', 'event1.jpg', 1),
+    (0, 'Music Fest', 2, '2023-11-06 11:00:00', 'Park Arena', 'event2.jpg', 2),
+    (0, 'Art Exhibition', 3, '2023-11-07 12:00:00', 'Art Gallery', 'event3.jpg', 3),
+    (0, 'Theater Premiere', 4, '2023-11-08 13:00:00', 'City Theater', 'event4.jpg', 4),
+    (0, 'City Parade', 5, '2023-11-09 14:00:00', 'Downtown', 'event5.jpg', 5),
+    (0, 'Tech Conference', 1, '2023-11-10 15:00:00', 'Tech Park', 'event6.jpg', 6),
+    (0, 'Live Jazz Night', 2, '2023-11-11 16:00:00', 'Jazz Club', 'event7.jpg', 7),
+    (0, 'Historical Exhibit', 3, '2023-11-12 17:00:00', 'History Museum', 'event8.jpg', 8),
+    (0, 'Drama Workshop', 4, '2023-11-13 18:00:00', 'Community Center', 'event9.jpg', 9),
+    (0, 'Summer Carnival', 5, '2023-11-14 19:00:00', 'Fairgrounds', 'event10.jpg', 10);
---
+++ KasVykstaManoMieste/FillTable/FillTable_EventCategory.sql
@@ -0,0 +1,7 @@
+INSERT INTO EventCategory (eventCategoryId, eventCategoryName)
+VALUES
+    (0, 'Educational'),
+    (0, 'Music'),
+    (0, 'Exhibition'),
+    (0, 'Theatre'),
+    (0, 'Parade');
